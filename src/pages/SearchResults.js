1493062ab31de5905ce922d480d92c00598749ec
ajtw7 <andrew.worsley91@gmail.com>
2024-02-05 23:33:10 -0500

    moved searchResults GET into seperate file; imported results call via useInfiniteQuery hook w/ pagination and conditional rendoring. Results limited to six atm; implemented 'load more' btn below results; 'prev' btn currently nonfunctional.

--- src/pages/SearchResults.js
+++ src/pages/SearchResults.js
@@ -1,62 +1,94 @@
-import { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { useInfiniteQuery } from 'react-query';
 import { useParams, Link } from 'react-router-dom';
+import { fetchResults } from '../services/resultsService';
 
 const SearchResults = () => {
-  const [results, setResults] = useState([]);
-
   const params = useParams();
-
-  // const location = useLocation();
-  // console.log(props)
-  useEffect(() => {
-    fetch(`https://openlibrary.org/search.json?title=${params.searchTerm}`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data, 'DATA');
-        // console.log(data.docs);
-        setResults(data.docs);
-      })
-      // .then(json => setSearchResults(json))
-      .catch((err) => console.log(err));
-  }, []);
-
-  return (
-    <div className="search-results-landing">
-      <h1>Search Results</h1>
-      <hr />
-      <div className="search-results-container">
-        {results &&
-          results.map((result) => {
-            return (
-              <div className="search-results-return">
-                {result.cover_edition_key && (
-                  <div className="individual-result">
-                    <h4>{result.title}</h4>
-                    <h4>
-                      {result.author_name && result.author_name.join(', ')}
-                    </h4>
-
-                    <img
-                      id="book-image"
-                      alt="cover not found"
-                      src={`https://covers.openlibrary.org/b/id/${result.cover_i}-L.jpg`}
-                    />
-
-                    <Link
-                      to={`/book-details/${result.key.slice(7)}/${
-                        result.cover_edition_key
-                      }`}
-                    >
-                      Details
-                    </Link>
-                  </div>
-                )}
-              </div>
-            );
-          })}
-      </div>
-    </div>
+  const [page, setPage] = useState(1);
+  const {
+    data: results,
+    error,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+    status,
+  } = useInfiniteQuery(
+    ['results', { searchTerm: params.searchTerm }],
+    fetchResults,
+    {
+      keepPreviousData: true,
+      getNextPageParam: (lastPage, results) => {
+        // results is an arr of all page data fetched thus far
+        const allPages = results.flatMap((page) => page.results);
+        if (lastPage.numFound > results.length) {
+          return allPages.length + 1;
+        }
+        console.log('pages:', results);
+        console.log('lastPage:', lastPage);
+        return undefined;
+      },
+    }
   );
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  } else if (status === 'error') {
+    return <div>Error: {error.message}</div>;
+  } else if (status === 'success') {
+    return (
+      <div className="search-results-landing">
+        <h1>Search Results</h1>
+        <hr />
+        <div className="search-results-container">
+          {results.pages.flatMap(page => page.results).map(result => (
+                <div
+                  className="search-results-return"
+                  key={result.cover_edition_key}
+                >
+                  {result.cover_edition_key && (
+                    <div className="individual-result" key={result.cover_edition_key}>
+                      <h4>{result.title}</h4>
+                      <h4>
+                        {result.author_name && result.author_name.join(', ')}
+                      </h4>
+
+                      <img
+                        id="book-image"
+                        alt="cover not found"
+                        src={`https://covers.openlibrary.org/b/id/${result.cover_i}-L.jpg`}
+                      />
+                      <Link
+                        to={`/book-details/${result.key.slice(7)}/${
+                          result.cover_edition_key
+                        }`}
+                      >
+                        Details
+                      </Link>
+                    </div>
+                  )}
+                </div>
+              ))}
+
+          <div>
+            <button
+              onClick={() => setPage((old) => Math.max(old - 1, 0))}
+              disabled={page === 1}
+            >
+              Previous Page
+            </button>
+            <button onClick={() => fetchNextPage()} disabled={!hasNextPage}>
+              {isFetchingNextPage
+                ? 'Loading more...'
+                : hasNextPage
+                ? 'Load More'
+                : 'Nothing more to load'}
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default SearchResults;
